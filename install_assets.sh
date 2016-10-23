fdc2ec19f016c4407a446c4fea95087d8af6b76e
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-23 17:10:55 +0200

    remove install_assets command

--- install_assets.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# install vendor assets (js/css) for bootstrap 4
-#
-
-ln -sf $(pwd)/vendor/twbs/bootstrap/dist/css $(pwd)/web/css
-ln -sf $(pwd)/vendor/twbs/bootstrap/dist/js $(pwd)/web/js
